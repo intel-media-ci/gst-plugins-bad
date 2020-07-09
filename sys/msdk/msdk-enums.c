a4cb48794cc2b7457cab7ad82978f3dadd802a72
Haihao Xiang <haihao.xiang@intel.com>
2020-07-09 14:46:58 +0800

    msdkenc: support tune property in msdkh264enc and msdkh265enc
    Introduce a new property for encoding mode selection, the default value
    for this new property allows the SDK to decide what to do. In addition,
    low-power is marked as deprecated since this fix

--- sys/msdk/msdk-enums.c
+++ sys/msdk/msdk-enums.c
@@ -176,6 +176,25 @@ gst_msdkenc_adaptive_b_get_type (void)
   return type;
 }
 
+GType
+gst_msdkenc_tune_mode_get_type (void)
+{
+  static GType type = 0;
+
+  static const GEnumValue values[] = {
+    {MFX_CODINGOPTION_UNKNOWN, "SDK decides what to do ", "auto"},
+    {MFX_CODINGOPTION_OFF, "Disable low power mode ", "off"},
+    {MFX_CODINGOPTION_ON, "Enable low power mode ", "on"},
+    {0, NULL, NULL}
+  };
+
+  if (!type) {
+    type = g_enum_register_static ("GstMsdkEncTuneMode", values);
+  }
+
+  return type;
+}
+
 /*========= MSDK VPP Enums =========================*/
 
 #ifndef GST_REMOVE_DEPRECATED
